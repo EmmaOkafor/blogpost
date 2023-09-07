3e9096a6072e9cf2ada5339ba796b0874109fad4
EmmaOkafor <okaforifebuche26@gmail.com>
2023-09-07 13:07:37 +0100

    Updates

--- src/components/temps/Ads.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Ads() {
-  return (
-    <div className='ads center'>
-        <h4>Advert</h4>
-    </div>
-  )
-}
--- src/components/temps/CatName.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CatName(props) {
-  return (
-    <div>
-        <p>{props.name}</p>
-    </div>
-  )
-}
